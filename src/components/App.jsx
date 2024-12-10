9c23bb83acfec3c37fdad88a13826ffbbfb85a0e
shafayat666 <shafayatmahin5@gmail.com>
2024-12-10 08:53:27 +0600

    added notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,18 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map((note) => (
+                <Note 
+                    title={note.title}
+                    content={note.content}
+                />
+            ))}
             <Footer />
         </div>
     );
